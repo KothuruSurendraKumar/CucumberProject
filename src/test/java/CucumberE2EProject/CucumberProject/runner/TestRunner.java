cc8bf065ff44a421c25c6215541391cf6c2820fa
SurendraKumar <surendrakumarsuri38@gmail.com>
2025-02-20 10:26:26 +0530

    New Tag Added

--- src/test/java/CucumberE2EProject/CucumberProject/runner/TestRunner.java
+++ src/test/java/CucumberE2EProject/CucumberProject/runner/TestRunner.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(features="src/test/resources/features",glue={"CucumberE2EProject.CucumberProject.stepImpl","CucumberE2EProject.CucumberProject.hooks"},
-monochrome=true,publish=true,plugin = {"pretty", "html:target/cucumber-reports.html","json:target/cucumber.json"},tags="@Login")
+monochrome=true,publish=true,plugin = {"pretty", "html:target/cucumber-reports.html","json:target/cucumber.json"},tags="@Search")
 
 public class TestRunner extends AbstractTestNGCucumberTests{
 
